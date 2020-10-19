958509c2be21bb151e63fd254042975e45781efb
Pavlov Vitalii <pavlov-vv86@yandex.ru>
2020-10-20 02:11:26 +0300

    правки

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,15 @@ const path = require("path");
 
 module.exports = {
   entry: [
-
+    "./js/backend.js",
     "./js/util.js",
     "./js/wizard.js",
     "./js/setup.js",
     "./js/render.js",
     "./js/dialog.js",
     "./js/move.js",
-    "./js/stat.js"
+    "./js/stat.js",
+    "./js/game.js"
   ],
   output: {
     filename: "bundle.js",
