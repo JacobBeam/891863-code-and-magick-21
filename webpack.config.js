67035b586e05c453c8c64e38085e49e5af5bc6fd
Pavlov Vitalii <pavlov-vv86@yandex.ru>
2020-10-20 00:53:03 +0300

    очередность подключения файлов

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,14 @@ const path = require("path");
 module.exports = {
   entry: [
     "./js/backend.js",
-    "./js/dialog.js",
-    "./js/game.js",
-    "./js/move.js",
-    "./js/render.js",
-    "./js/setup.js",
-    "./js/stat.js",
     "./js/util.js",
-    "./js/wizard.js"
+    "./js/wizard.js",
+    "./js/setup.js",
+    "./js/render.js",
+    "./js/dialog.js",
+    "./js/move.js",
+    "./js/stat.js",
+    "./js/game.js"
   ],
   output: {
     filename: "bundle.js",
